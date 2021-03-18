f2b812f349fcf21add499bff0d034e81c1140e77
tsyhaniuk_nazar <tsyhanyuknazar@gmail.com>
2021-03-18 16:33:30 +0100

    add project

---
+++ server/src/study/chat/server/ChatServer.java
@@ -0,0 +1,64 @@
+package study.chat.server;
+
+import study.chat.client.TCPConnection;
+import study.chat.client.TCPConnectionListener;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+
+public class ChatServer implements TCPConnectionListener {
+
+    public static void main(String[] args) {
+        new ChatServer();
+    }
+
+    private final ArrayList<TCPConnection> connections = new ArrayList<>();
+
+    private ChatServer() {
+        System.out.println("Server running...");
+
+        try (ServerSocket serverSocket = new ServerSocket(8189)) {
+            while (true) {
+                try {
+                    new TCPConnection(this, serverSocket.accept());
+                }catch (IOException e) {
+                    System.out.println("TCPConnection exception " + e);
+                }
+            }
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public synchronized void onConnectionReady(TCPConnection tcpConnection) {
+        connections.add(tcpConnection);
+        sentToAllConnections("Client connected: " + tcpConnection);
+    }
+
+    @Override
+    public synchronized void onReceiveString(TCPConnection tcpConnection, String value) {
+        sentToAllConnections(value);
+    }
+
+    @Override
+    public synchronized void onDisconnect(TCPConnection tcpConnection) {
+        connections.remove(tcpConnection);
+        sentToAllConnections("Client disconnected: " + tcpConnection);
+    }
+
+    @Override
+    public synchronized void onException(TCPConnection tcpConnection, Exception e) {
+        System.out.println("TCPConnection exception: " + e);
+    }
+
+    private void sentToAllConnections(String value) {
+        System.out.println(value);
+
+        for (int i = 0; i < connections.size(); i++) {
+            connections.get(i).sendMessage(value);
+        }
+    }
+}
